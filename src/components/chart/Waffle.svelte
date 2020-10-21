0f70f5df2696e260e644a3d76d2ab71551f3f802
Giles Dring <giles@dringtech.com>
2020-10-21 13:43:14 +0100

    Styling

--- src/components/chart/Waffle.svelte
+++ src/components/chart/Waffle.svelte
@@ -5,12 +5,12 @@
 
 <svg viewbox="0 0 120 120">
   {#each Array(100) as _, cell}
-    <rect class:greyed={ cell > 100*number/total } width="10" height="10" x={12 * ((99-cell) % 10)} y={12 * Math.floor((99-cell) / 10)}></rect>
+    <rect class:greyed={ cell > 100*number/total } width="10" height="10" x={12 * (cell % 10)} y={12 * Math.floor((99-cell) / 10)}></rect>
   {/each}
 </svg>
 
 <style type='text/scss'>
   .greyed {
-    fill: #ccc;
+    fill: #eee;
   }
 </style>
