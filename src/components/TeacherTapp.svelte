d26adf165ad9ba6c6ed4228b535887c7d7dd069d
Giles Dring <giles@dringtech.com>
2020-10-27 19:20:52 +0000

    feat: add comparator for Teacher Tapp numbers

--- src/components/TeacherTapp.svelte
+++ src/components/TeacherTapp.svelte
@@ -1,11 +1,12 @@
 <script>
-  import { getJson} from '../lib/fetch';
+  import { getJson } from '../lib/fetch';
   import { formatDate } from '../lib/utils';
   import Waffle from './chart/Waffle.svelte';
 
   const dataFile = 'data/teacher-tapp/questions.json';
   const loadData = getJson(dataFile);
   let dimension = 0;
+  let comparator = undefined;
 </script>
 
 <section class="teachertapp">
@@ -14,19 +15,32 @@
   {#await loadData}
     <p>Loading data file</p>
   {:then questions}
-    <label for="tt-dimension">Choose a dimension:</label>
-    <select id="tt-dimension" bind:value={dimension}>
-      {#each questions.k as dim, i}
-        <option value={ i }>{ dim }</option>
-      {/each}
-    </select>
+    <div>
+      <label for="tt-dimension">Choose a primary dimension</label>
+      <select id="tt-dimension" bind:value={dimension}>
+        {#each questions.k as dim, i}
+          <option value={ i }>{ dim }</option>
+        {/each}
+      </select>
+    </div>
+    {#if dimension != 0}
+      <div>
+        <label for="tt-dimension">Choose a dimension to compare against</label>
+        <select id="tt-dimension" bind:value={comparator}>
+          <option>None</option>
+          {#each questions.k as dim, i}
+            <option value={ i }>{ dim }</option>
+          {/each}
+        </select>
+      </div>
+    {/if}
 
     {#each questions.q as question}
       <h3>{ question.q }</h3>
       <div class="grid">
         {#each question.a as response }
           <section class="response">
-            <Waffle number={ response.d[dimension] } />
+            <Waffle number={ response.d[dimension] } reference={ dimension > 0 ? response.d[comparator] : undefined }/>
             <p>{ response.t }</p>
           </section>
         {/each}
@@ -43,7 +57,7 @@
     flex-wrap: wrap;
   }
   .response {
-    width: 5em;
+    width: 10em;
     padding-bottom: 1em;
     padding-right: 1em;
     p {
--- src/components/chart/Waffle.svelte
+++ src/components/chart/Waffle.svelte
@@ -1,11 +1,15 @@
 <script>
   export let number;
+  export let reference;
   export let total = 100;
 </script>
 
 <svg viewbox="0 0 120 120">
   {#each Array(100) as _, cell}
-    <rect class:greyed={ cell + 1 > 100*number/total } width="10" height="10" x={12 * (cell % 10)} y={12 * Math.floor((99-cell) / 10)}></rect>
+    <rect
+      class:greyed={ cell + 1 > 100*number/total }
+      class:baseline={ cell + 1 <= 100*reference/total }
+      width="10" height="10" x={12 * (cell % 10)} y={12 * Math.floor((99-cell) / 10)}></rect>
   {/each}
 </svg>
 
