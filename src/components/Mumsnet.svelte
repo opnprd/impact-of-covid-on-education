9d15a1d908bd1a1ef86ed418b0b8525eef88f6ae
Giles Dring <giles@dringtech.com>
2020-10-29 13:15:57 +0000

    Adding Mumsnet analysis

--- src/components/Mumsnet.svelte
+++ src/components/Mumsnet.svelte
@@ -1,26 +1,41 @@
 <script>
-  import Table from "./Table.svelte";
-  import { getCsv } from "../lib/fetch";
-  const dataFile = "data/mumsnet/forum-terms.csv";
-  let filterColumns;
-  let column;
+  import Table from './Table.svelte';
+  import { getCsv } from '../lib/fetch';
+  let column = 2;
+  let dataset = 0;
+  let datasets = ['tokens', 'bigrams'];
   let data;
   let rawData;
+
+  function extractData(raw, index, sortColumn) {
+    const result = raw[index];
+    const col = result.columns[sortColumn];
+    const sorter = sortColNumeric(col);
+    const shortset = result.sort(sorter).slice(0, 10);
+    shortset.columns = result.columns;
+    return shortset;
+  }
+
   async function getData() {
-    rawData = await getCsv(dataFile);
-    filterColumns = rawData.columns.slice(1);
-    column = filterColumns[1];
+    rawData = await Promise.all(
+      datasets.map((x) => getCsv(`data/mumsnet/${x}.csv`))
+    );
+    data = extractData(rawData, dataset, column);
   }
   let loadData = getData();
   $: if (rawData) {
-    data = rawData.sort(dataSort(column)).slice(0, 10);
-    data.columns = rawData.columns;
+    data = extractData(rawData, dataset, column);
   }
 
-  const dataSort = (c) => (a, b) => {
+  const sortColNumeric = (c) => (a, b) => {
     if (parseInt(a[c]) > parseInt(b[c])) return -1;
     if (parseInt(a[c]) < parseInt(b[c])) return 1;
     return 0;
+  };
+
+  const handleClick = (c) => {
+    if (c === 0) return;
+    column = c;
   };
 </script>
 
@@ -28,20 +43,29 @@
 </style>
 
 <section class="mumsnet">
-  <h2>Mumsnet</h2>
+  <h2>The Parent's Perspective</h2>
+  <article>
+    <p>
+      We have performed some high-level analysis on terms mentioned in
+      education-related Mumsnet posts during January, April and September. The
+      top terms or bigrams are presented in the table below, showing] how
+      prevalence of particlar terms has changed over time.
+    </p>
+  </article>
 
-  <p>Top terms from eduation-related posts in Mumsnet MI forums.</p>
   {#await loadData}
     Loading data...
   {:then}
-    {#each filterColumns as option, i}
-      <label for="col_{i}">{option}</label>
-      <input id="col_{i}" type="radio" bind:group={column} value={option} />
-    {/each}
-    <Table {data} />
+    <select bind:value={dataset}>
+      {#each datasets as opt, i}
+        <option value={ i }>{ opt.charAt(0).toUpperCase() + opt.slice(1) }</option>
+      {/each}
+    </select>
+
+    <Table {data} handler={handleClick} highlight={column} />
     <aside>Data sourced from Mumsnet MI systems with kind permission.</aside>
   {:catch e}
-    <p style="color: red">{dataFile} not found!</p>
+    <p style="color: red">Data not loaded!</p>
     <p>{e}</p>
   {/await}
 </section>
--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,6 +1,8 @@
 <script>
   import Circle from "./Circle.svelte";
   export let data;
+  export let handler = () => {};
+  export let highlight;
   const numeric = (n) => !isNaN(n);
   $: max = data.reduce((accumulator, n) => {
     const current = Math.max(...Object.values(n).filter(numeric));
@@ -8,6 +10,31 @@
     return accumulator;
   }, 0);
 </script>
+
+<table>
+  <thead>
+    <tr>
+      {#each data.columns as col, i}
+        <th class:selected={ i === highlight } on:click="{() => handler(i)}">{col}</th>
+      {/each}
+    </tr>
+  </thead>
+  <tbody>
+    {#each data as row}
+      <tr>
+        {#each data.columns as col, i}
+          <td class:selected={ i === highlight }>
+            {#if numeric(row[col])}
+              <Circle
+                radius={Math.sqrt(row[col] / max)}
+                content="<p>{row[col]}</p>" />
+            {:else}{row[col]}{/if}
+          </td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
 
 <style type="text/scss">
   table {
@@ -22,28 +49,3 @@
     }
   }
 </style>
-
-<table>
-  <thead>
-    <tr>
-      {#each data.columns as col}
-        <th>{col}</th>
-      {/each}
-    </tr>
-  </thead>
-  <tbody>
-    {#each data as row}
-      <tr>
-        {#each data.columns as col}
-          <td>
-            {#if numeric(row[col])}
-              <Circle
-                radius={Math.sqrt(row[col] / max)}
-                content="<p>{row[col]}</p>" />
-            {:else}{row[col]}{/if}
-          </td>
-        {/each}
-      </tr>
-    {/each}
-  </tbody>
-</table>
