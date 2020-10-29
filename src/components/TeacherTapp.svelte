4f436535d3c3d0d410291b216f622e9d4f28b42e
Giles Dring <giles@dringtech.com>
2020-10-29 13:20:22 +0000

    Adding latest Teacher Tapp results

--- src/components/TeacherTapp.svelte
+++ src/components/TeacherTapp.svelte
@@ -9,48 +9,6 @@
   let comparator = undefined;
 </script>
 
-<section class="teachertapp">
-  <h2>Teacher Tapp</h2>
-
-  {#await loadData}
-    <p>Loading data file</p>
-  {:then questions}
-    <div>
-      <label for="tt-dimension">Choose a primary dimension</label>
-      <select id="tt-dimension" bind:value={dimension}>
-        {#each questions.k as dim, i}
-          <option value={ i }>{ dim }</option>
-        {/each}
-      </select>
-    </div>
-    {#if dimension != 0}
-      <div>
-        <label for="tt-dimension">Choose a dimension to compare against</label>
-        <select id="tt-dimension" bind:value={comparator}>
-          <option>None</option>
-          {#each questions.k as dim, i}
-            <option value={ i }>{ dim }</option>
-          {/each}
-        </select>
-      </div>
-    {/if}
-
-    {#each questions.q as question}
-      <h3>{ question.q }</h3>
-      <div class="grid">
-        {#each question.a as response }
-          <section class="response">
-            <Waffle number={ response.d[dimension] } reference={ dimension > 0 ? response.d[comparator] : undefined }/>
-            <p>{ response.t }</p>
-          </section>
-        {/each}
-      </div>
-      <p>Total respondents { question.r }</p>
-      <p>Question asked on { formatDate(question.d) }</p>
-    {/each}
-  {/await}
-</section>
-
 <style type="text/scss">
   .grid {
     display: flex;
@@ -62,7 +20,53 @@
     padding-right: 1em;
     p {
       font-size: 0.7em;
-      margin: 0; 
+      margin: 0;
     }
   }
 </style>
+
+<section class="teachertapp">
+  <h2>The Teacher Perspective</h2>
+  <section>
+    <p>We surveyed a group of teachers using the Teacher Tapp application.</p>
+  </section>
+  {#await loadData}
+    <p>Loading data file</p>
+  {:then questions}
+    <div>
+      <label for="tt-dimension">Choose a primary dimension</label>
+      <select id="tt-dimension" bind:value={dimension}>
+        {#each questions.k as dim, i}
+          <option value={i}>{dim}</option>
+        {/each}
+      </select>
+    </div>
+    {#if dimension != 0}
+      <div>
+        <label for="tt-dimension">Choose a dimension to compare against</label>
+        <select id="tt-dimension" bind:value={comparator}>
+          <option>None</option>
+          {#each questions.k as dim, i}
+            <option value={i}>{dim}</option>
+          {/each}
+        </select>
+      </div>
+    {/if}
+
+    {#each questions.q as question}
+      <h3>{question.q}</h3>
+      <div class="grid">
+        {#each question.a as response}
+          <section class="response">
+            <Waffle
+              number={response.d[dimension]}
+              reference={dimension > 0 ? response.d[comparator] : undefined} />
+            <p>{response.t}</p>
+          </section>
+        {/each}
+      </div>
+      <p>Total respondents {question.r}</p>
+      <p>Question asked on {formatDate(question.d)}</p>
+    {/each}
+  {/await}
+</section>
