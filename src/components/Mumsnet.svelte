4e456b6df492de4f72b5a5fcab5c03f557ec34a9
Giles Dring <giles@dringtech.com>
2020-11-05 17:09:13 +0000

    Fixing display of Mumsnet data

--- src/components/Mumsnet.svelte
+++ src/components/Mumsnet.svelte
@@ -9,11 +9,11 @@
   let rawData;
   let slice = 10;
 
-  function extractData(raw, index, sortColumn) {
+  function extractData(raw, index, sortColumn, count) {
     const result = raw[index];
     const col = result.columns[sortColumn];
     const sorter = sortColNumeric(col);
-    const shortset = result.sort(sorter).slice(0, slice);
+    const shortset = result.sort(sorter).slice(0, count);
     shortset.columns = result.columns;
     return shortset;
   }
@@ -22,16 +22,18 @@
     rawData = await Promise.all(
       datasets.map((x) => getCsv(`data/mumsnet/${x}.csv`))
     );
-    data = extractData(rawData, dataset, column + 1);
+    data = extractData(rawData, dataset, column + 1, slice);
   }
   let loadData = getData();
   $: if (rawData) {
-    data = extractData(rawData, dataset, column + 1);
+    data = extractData(rawData, dataset, column + 1, slice);
   }
 
   const sortColNumeric = (c) => (a, b) => {
     if (parseInt(a[c]) > parseInt(b[c])) return -1;
     if (parseInt(a[c]) < parseInt(b[c])) return 1;
+    if (a['term'] < b['term']) return -1;
+    if (a['term'] > b['term']) return 1;
     return 0;
   };
 
@@ -45,12 +47,18 @@
   {#await loadData}
     Loading data...
   {:then}
-    <label for="token-selector">Select a type of term:</label>
-    <select id="token-selector" bind:value={dataset}>
-      {#each datasets as opt, i}
-        <option value={i}>{opt.charAt(0).toUpperCase() + opt.slice(1)}</option>
-      {/each}
-    </select>
+    <div>
+      <label for="token-selector">Select a type of term:</label>
+      <select id="token-selector" bind:value={dataset}>
+        {#each datasets as opt, i}
+          <option value={i}>{opt.charAt(0).toUpperCase() + opt.slice(1)}</option>
+        {/each}
+      </select>
+    </div>
+    <div>
+      <label for='token-count'>How many terms to show?</label>
+      <input id='token-count' type='range' min=5 max=30 bind:value={slice} />
+    </div>
 
     <p>
       Click the date header to select the
